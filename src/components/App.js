ac3382d09f01c624bccd4c03973168f39443542c
Muhammad Tameem Shahid <tameem_shahid@yahoo.com>
2021-07-11 15:50:11 +0500

    Refactored login component

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,13 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import ParticleComponent from "./ParticleComponent";
 
 function App() {
+  // TOPOLOGY CONFIGURATION
   const [data, setData] = useState({
     nodes: [{ id: "node0" }, { id: "node1" }],
     links: [{ source: "node0", target: "node1" }],
   });
 
-  const [ViewTopo, setViewTopo] = useState(true);
-  const [ViewTitlePage, setViewTitlePage] = useState(true);
-  const [ViewLogin, setViewLogin] = useState(true);
-  const ViewLoginForm = () => {
-    setViewTitlePage(false);
-    setViewLogin(true);
-  };
+  // GRAPH MODULE CONFIGURATION
 
   const myConfig = {
     nodeHighlightBehavior: true,
@@ -33,106 +28,96 @@ function App() {
     },
   };
 
-  const onClickNode = function (nodeId) {
-    console.log(nodeId);
-  };
-
-  const onClickLink = function (source, target) {
-    window.alert(`Clicked link between ${source} and ${target}`);
-  };
-
-  const [nodesNum, setNodesNum] = useState(data.nodes.length);
-  const [linkInput, setLinkInput] = useState({
-    sourceInput: "",
-    destinationInput: "",
-  });
-
-  const [loginInput, setLoginInput] = useState({
-    username: "",
-    password: "",
-  });
-
-  const [nodeCordinates, setNodeCordinates] = useState({ x: 20, y: 20 });
-  const addNode = (memory, radius, cache, angle, cpu) => {
-    console.log(memory, radius, cache, angle, cpu);
-    setNodesNum(nodesNum + 1);
-    const nodes = data.nodes.concat({
-      id: `node${nodesNum}`,
-      x: nodeCordinates.x,
-      y: nodeCordinates.y,
-      memory,
-      radius,
-      cache,
-      angle,
-      cpu,
-    });
-    setData({ nodes: nodes, links: data.links });
-    setNodeCordinates({ x: nodeCordinates.x + 5, y: nodeCordinates.y + 10 });
-  };
-
-  const addLink = (bandwidth, delay, loss) => {
-    const var1 = data.nodes.find((node) => node.id === linkInput.sourceInput);
-    const var2 = data.nodes.find(
-      (node) => node.id === linkInput.destinationInput
-    );
-    const var3 = data.links.find(
-      (link) =>
-        link.source === linkInput.sourceInput &&
-        link.target === linkInput.destinationInput
-    );
-    if (var1 && var2) {
-      if (var3) {
-        window.alert("Link already exist!");
-      } else {
-        const links = data.links.concat({
-          source: linkInput.sourceInput,
-          target: linkInput.destinationInput,
-          bandwidth,
-          delay,
-          loss,
-        });
-        console.log(links);
-        setData({ nodes: data.nodes, links: links });
-      }
-    } else {
-      const string = var1 ? "Second" : "First";
-      window.alert(`${string} node does not exist`);
-    }
-  };
+  // LOGIN STATES AND FUNCTIONS
 
   const [authorized, setAuthorized] = useState(false);
-  const handleLogin = (history) => {
-    axios
-      .post("http://localhost:3001/persons", loginInput)
-      .then((response) => {
-        if (response.status === 200) {
-          setViewBuilder(true);
-          setAuthorized(true);
-          history.push("/build");
-        } else if (response.status === 204) {
-          throw Error("User name or Password incorrect!");
-        }
-      })
-      .catch((error) => {
-        window.alert(error);
-      });
-  };
+
+  // const [ViewTopo, setViewTopo] = useState(true);
+  // const [ViewTitlePage, setViewTitlePage] = useState(true);
+  // const [ViewLogin, setViewLogin] = useState(true);
+  // const ViewLoginForm = () => {
+  //   setViewTitlePage(false);
+  //   setViewLogin(true);
+  // };
+
+  // const onClickNode = function (nodeId) {
+  //   console.log(nodeId);
+  // };
+
+  // const onClickLink = function (source, target) {
+  //   window.alert(`Clicked link between ${source} and ${target}`);
+  // };
+
+  // const [nodesNum, setNodesNum] = useState(data.nodes.length);
+  // const [linkInput, setLinkInput] = useState({
+  //   sourceInput: "",
+  //   destinationInput: "",
+  // });
+
+  // const [loginInput, setLoginInput] = useState({
+  //   username: "",
+  //   password: "",
+  // });
+
+  // const [nodeCordinates, setNodeCordinates] = useState({ x: 20, y: 20 });
+  // const addNode = (memory, radius, cache, angle, cpu) => {
+  //   console.log(memory, radius, cache, angle, cpu);
+  //   setNodesNum(nodesNum + 1);
+  //   const nodes = data.nodes.concat({
+  //     id: `node${nodesNum}`,
+  //     x: nodeCordinates.x,
+  //     y: nodeCordinates.y,
+  //     memory,
+  //     radius,
+  //     cache,
+  //     angle,
+  //     cpu,
+  //   });
+  //   setData({ nodes: nodes, links: data.links });
+  //   setNodeCordinates({ x: nodeCordinates.x + 5, y: nodeCordinates.y + 10 });
+  // };
+
+  // const addLink = (bandwidth, delay, loss) => {
+  //   const var1 = data.nodes.find((node) => node.id === linkInput.sourceInput);
+  //   const var2 = data.nodes.find(
+  //     (node) => node.id === linkInput.destinationInput
+  //   );
+  //   const var3 = data.links.find(
+  //     (link) =>
+  //       link.source === linkInput.sourceInput &&
+  //       link.target === linkInput.destinationInput
+  //   );
+  //   if (var1 && var2) {
+  //     if (var3) {
+  //       window.alert("Link already exist!");
+  //     } else {
+  //       const links = data.links.concat({
+  //         source: linkInput.sourceInput,
+  //         target: linkInput.destinationInput,
+  //         bandwidth,
+  //         delay,
+  //         loss,
+  //       });
+  //       console.log(links);
+  //       setData({ nodes: data.nodes, links: links });
+  //     }
+  //   } else {
+  //     const string = var1 ? "Second" : "First";
+  //     window.alert(`${string} node does not exist`);
+  //   }
+  // };
 
   const createTopology = (history) => {
     axios
       .post("http://localhost:3001/topology", data)
       .then(() => {
         console.log("Data send to backend");
-        setViewBuilder(false);
-        setViewTopo(true);
         history.push("/view");
       })
       .catch((error) => {
         window.alert(error);
       });
   };
-
-  const [viewBuilder, setViewBuilder] = useState(true);
 
   return (
     <div
@@ -149,49 +134,25 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/view">
-            <TopoViewer
-              data={data}
-              myConfig={myConfig}
-              onClickLink={onClickLink}
-              onClickNode={onClickNode}
-            ></TopoViewer>
+            <TopoViewer data={data} myConfig={myConfig}></TopoViewer>
           </Route>
           <Route path="/build">
             {authorized ? (
               <TopoBuilder
-                data={data}
-                addNode={addNode}
-                addLink={addLink}
-                linkInput={linkInput}
-                setLinkInput={setLinkInput}
+                topoData={data}
                 createTopology={createTopology}
                 myConfig={myConfig}
-                onClickLink={onClickLink}
-                onClickNode={onClickNode}
-                ViewTopo={ViewTopo}
-                viewBuilder={viewBuilder}
-                username={loginInput.username}
+                // username={loginInput.username}
               ></TopoBuilder>
             ) : (
               <Redirect to="/login"></Redirect>
             )}
           </Route>
           <Route path="/login">
-            <Login
-              loginInput={loginInput}
-              setLoginInput={setLoginInput}
-              handleLogin={handleLogin}
-              viewBuilder={viewBuilder}
-              setViewBuilder={setViewBuilder}
-              ViewTopo={ViewTopo}
-              ViewLogin={ViewLogin}
-            ></Login>
+            <Login setAuthorized={setAuthorized}></Login>
           </Route>
           <Route path="/">
-            <TitlePage
-              ViewTitlePage={ViewTitlePage}
-              ViewLoginForm={ViewLoginForm}
-            ></TitlePage>
+            <TitlePage></TitlePage>
           </Route>
         </Switch>
       </BrowserRouter>
--- src/components/TopoBuilder.js
+++ src/components/TopoBuilder.js
@@ -12,24 +12,16 @@ import {
   Modal,
 } from "react-bootstrap";
 
-const TopoBuilder = ({
-  data,
-  addNode,
-  addLink,
-  linkInput,
-  setLinkInput,
-  createTopology,
-  myConfig,
-  onClickLink,
-  onClickNode,
-  ViewTopo,
-  viewBuilder,
-  username,
-}) => {
+const TopoBuilder = ({ topoData, createTopology, myConfig }) => {
   const history = useHistory();
+
+  // STATES
+  const [data, setData] = useState(topoData);
   const [makeTopo, setMakeTopo] = useState(false);
   const [showAlert, setShowAlert] = useState(true);
-
+  /*
+    WELCOME ALERT MESSAGE
+  */
   const alertMessage = () => {
     if (showAlert) {
       return (
@@ -39,7 +31,7 @@ const TopoBuilder = ({
           dismissible
           transition
         >
-          Welcome {username}!
+          Welcome!
         </Alert>
       );
     } else {
@@ -47,16 +39,87 @@ const TopoBuilder = ({
     }
   };
 
+  // FUNCTIONS
+
+  const onClickNode = function (nodeId) {
+    console.log(nodeId);
+  };
+
+  const onClickLink = function (source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };
+
+  /*
+    ADD NODE HANDLER
+  */
+  const [nodesNum, setNodesNum] = useState(data.nodes.length);
+  const [nodeCordinates, setNodeCordinates] = useState({ x: 20, y: 20 });
+  const addNode = () => {
+    console.log(
+      `Memory: ${nodeConfig.memory}, radius: ${nodeConfig.radius}, cache: ${nodeConfig.cache}, angle: ${nodeConfig.angle}, cpu: ${nodeConfig.cpu}`
+    );
+    setNodesNum(nodesNum + 1);
+    const nodes = data.nodes.concat({
+      id: `node${nodesNum}`,
+      x: nodeCordinates.x,
+      y: nodeCordinates.y,
+      memory: nodeConfig.memory,
+      radius: nodeConfig.radius,
+      cache: nodeConfig.cache,
+      angle: nodeConfig.angle,
+      cpu: nodeConfig.cpu,
+    });
+    setData({ nodes: nodes, links: data.links });
+    setNodeCordinates({ x: nodeCordinates.x + 5, y: nodeCordinates.y + 10 });
+  };
+
   /*
     ADD NODE MODAL STATES AND FUNCTION 
   */
 
   const [showNodeModal, setShowNodeModal] = useState(false);
-  const [memory, setMemory] = useState("");
-  const [radius, setRadius] = useState("");
-  const [cache, setCache] = useState("");
-  const [angle, setAngle] = useState("");
-  const [cpu, setCpu] = useState("");
+  const [nodeConfig, setNodeConfig] = useState({
+    memory: "",
+    radius: "",
+    cache: "",
+    angle: "",
+    cpu: "",
+  });
+
+  const handleNodeInputChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    var valid = false;
+    switch (name) {
+      case "memory":
+        valid = value > 0 && value <= 1024 * 1024 ? true : false;
+        break;
+      case "radius":
+        valid = value >= 0 && value <= 1.0 ? true : false;
+        break;
+      case "cache":
+        valid = value > 0 && value <= 1024 * 100 ? true : false;
+        break;
+      case "angle":
+        valid = value >= 0 && value <= 360 ? true : false;
+        break;
+      case "cpu":
+        valid = value >= 0 && value <= 1.0 ? true : false;
+        break;
+    }
+    if (!valid) {
+      window.alert(`Invalid ${name} value, please enter correct value.`);
+      setNodeConfig({
+        ...nodeConfig,
+        [name]: "",
+      });
+    } else {
+      setNodeConfig({
+        ...nodeConfig,
+        [event.target.name]: event.target.value,
+      });
+    }
+  };
 
   const nodeModal = () => {
     return (
@@ -74,55 +137,50 @@ const TopoBuilder = ({
           <InputGroup className="mb-3">
             <InputGroup.Text>Memory</InputGroup.Text>
             <FormControl
+              name="memory"
               placeholder="in KB's"
-              value={memory}
-              onChange={(event) => {
-                setMemory(event.target.value);
-              }}
+              value={nodeConfig.memory}
+              onChange={handleNodeInputChange}
             />
           </InputGroup>
           {/* RADIUS INPUT */}
           <InputGroup className="mb-3">
             <InputGroup.Text>Radius</InputGroup.Text>
             <FormControl
+              name="radius"
               placeholder="<0.0-1.0>"
-              value={radius}
-              onChange={(event) => {
-                setRadius(event.target.value);
-              }}
+              value={nodeConfig.radius}
+              onChange={handleNodeInputChange}
             />
           </InputGroup>
           {/* CACHE INPUT */}
           <InputGroup className="mb-3">
             <InputGroup.Text>Cache</InputGroup.Text>
             <FormControl
+              name="cache"
               placeholder="in KB's"
-              value={cache}
-              onChange={(event) => {
-                setCache(event.target.value);
-              }}
+              value={nodeConfig.cache}
+              onChange={handleNodeInputChange}
             />
           </InputGroup>
           {/* ANGLE INPUT */}
           <InputGroup className="mb-3">
             <InputGroup.Text>Angle</InputGroup.Text>
             <FormControl
+              name="angle"
               placeholder="<0-360>"
-              value={angle}
-              onChange={(event) => {
-                setAngle(event.target.value);
-              }}
+              value={nodeConfig.angle}
+              onChange={handleNodeInputChange}
             />
           </InputGroup>
           {/* CPU INPUT */}
           <InputGroup className="mb-3">
             <InputGroup.Text>CPU</InputGroup.Text>
             <FormControl
+              name="cpu"
               placeholder="<0.0-1.0>"
-              value={cpu}
-              onChange={(event) => {
-                setCpu(event.target.value);
-              }}
+              value={nodeConfig.cpu}
+              onChange={handleNodeInputChange}
             />
           </InputGroup>
         </Modal.Body>
@@ -133,13 +191,16 @@ const TopoBuilder = ({
           <Button
             variant="primary"
             onClick={() => {
-              addNode(memory, radius, cache, angle, cpu);
+              addNode();
+              console.log(nodeConfig);
               setShowNodeModal(false);
-              setAngle("");
-              setCache("");
-              setCpu("");
-              setMemory("");
-              setRadius("");
+              setNodeConfig({
+                memory: "",
+                radius: "",
+                cache: "",
+                angle: "",
+                cpu: "",
+              });
             }}
           >
             Add Node
@@ -147,6 +208,43 @@ const TopoBuilder = ({
         </Modal.Footer>
       </Modal>
     );
+  };
+
+  /*
+    ADD LINK STATES AND HANDLER
+  */
+  const [linkInput, setLinkInput] = useState({
+    sourceInput: "",
+    destinationInput: "",
+  });
+  const addLink = (bandwidth, delay, loss) => {
+    const var1 = data.nodes.find((node) => node.id === linkInput.sourceInput);
+    const var2 = data.nodes.find(
+      (node) => node.id === linkInput.destinationInput
+    );
+    const var3 = data.links.find(
+      (link) =>
+        link.source === linkInput.sourceInput &&
+        link.target === linkInput.destinationInput
+    );
+    if (var1 && var2) {
+      if (var3) {
+        window.alert("Link already exist!");
+      } else {
+        const links = data.links.concat({
+          source: linkInput.sourceInput,
+          target: linkInput.destinationInput,
+          bandwidth,
+          delay,
+          loss,
+        });
+        console.log(links);
+        setData({ nodes: data.nodes, links: links });
+      }
+    } else {
+      const string = var1 ? "Second" : "First";
+      window.alert(`${string} node does not exist`);
+    }
   };
 
   /*
@@ -229,126 +327,121 @@ const TopoBuilder = ({
   /*
     ADD LINK STATES AND FUNCTIONS
    */
-
-  if (viewBuilder === true) {
-    return (
-      <Container>
-        {alertMessage()}
-        {nodeModal()}
-        {linkModal()}
-        <h1 style={{ fontFamily: "Roboto", paddingTop: "20px" }}>TOPOLOGY</h1>
-        <Row style={makeTopo ? {} : { height: "85vh" }}>
-          <Col>
-            <Button
-              variant="secondary"
-              block
-              onClick={() => setMakeTopo(true)}
-              data-testid="create-topology-button"
-            >
-              Create Topology
-            </Button>{" "}
-          </Col>
-          <Col>
-            <Button
-              variant="secondary"
-              block
-              onClick={() => createTopology(history)}
-              data-testid="view-topology-button"
-            >
-              View Topology
-            </Button>{" "}
-          </Col>
-        </Row>
-        <br></br>
-        <Row className={makeTopo ? "" : "d-none"}>
-          <Col className="justify-content-right">
-            <br></br>
-            <br></br>
-            <br></br>
-            <Button
-              variant="secondary"
-              onClick={() => setShowNodeModal(true)}
-              block
-            >
-              Add Node
-            </Button>{" "}
-            <br></br>
-            <InputGroup className="">
-              <FormControl
-                aria-label="Source Node"
-                aria-describedby="basic-addon1"
-                value={linkInput.sourceInput}
-                placeholder="Source Node"
-                onChange={(event) => {
-                  setLinkInput({
-                    sourceInput: event.target.value,
-                    destinationInput: linkInput.destinationInput,
-                  });
-                }}
-              />
-            </InputGroup>
-            <InputGroup className="mb-1">
-              <FormControl
-                aria-label="Destination Node"
-                aria-describedby="basic-addon1"
-                value={linkInput.destinationInput}
-                placeholder="Destination Node"
-                onChange={(event) => {
-                  setLinkInput({
-                    destinationInput: event.target.value,
-                    sourceInput: linkInput.sourceInput,
-                  });
-                }}
-              />
-            </InputGroup>
-            <Button
-              variant="secondary"
-              onClick={() => setShowLinkModal(true)}
-              block
-            >
-              Add Link
-            </Button>{" "}
-            <br></br>
-            <br></br>
-            <br></br>
-            <Button
-              variant="secondary"
-              onClick={() => createTopology(history)}
-              block
-            >
-              Submit
-            </Button>{" "}
-            {/* <Button variant="secondary" block>
+  return (
+    <Container>
+      {alertMessage()}
+      {nodeModal()}
+      {linkModal()}
+      <h1 style={{ fontFamily: "Roboto", paddingTop: "20px" }}>TOPOLOGY</h1>
+      <Row style={makeTopo ? {} : { height: "85vh" }}>
+        <Col>
+          <Button
+            variant="secondary"
+            block
+            onClick={() => setMakeTopo(true)}
+            data-testid="create-topology-button"
+          >
+            Create Topology
+          </Button>{" "}
+        </Col>
+        <Col>
+          <Button
+            variant="secondary"
+            block
+            onClick={() => createTopology(history)}
+            data-testid="view-topology-button"
+          >
+            View Topology
+          </Button>{" "}
+        </Col>
+      </Row>
+      <br></br>
+      <Row className={makeTopo ? "" : "d-none"}>
+        <Col className="justify-content-right">
+          <br></br>
+          <br></br>
+          <br></br>
+          <Button
+            variant="secondary"
+            onClick={() => setShowNodeModal(true)}
+            block
+          >
+            Add Node
+          </Button>{" "}
+          <br></br>
+          <InputGroup className="">
+            <FormControl
+              aria-label="Source Node"
+              aria-describedby="basic-addon1"
+              value={linkInput.sourceInput}
+              placeholder="Source Node"
+              onChange={(event) => {
+                setLinkInput({
+                  sourceInput: event.target.value,
+                  destinationInput: linkInput.destinationInput,
+                });
+              }}
+            />
+          </InputGroup>
+          <InputGroup className="mb-1">
+            <FormControl
+              aria-label="Destination Node"
+              aria-describedby="basic-addon1"
+              value={linkInput.destinationInput}
+              placeholder="Destination Node"
+              onChange={(event) => {
+                setLinkInput({
+                  destinationInput: event.target.value,
+                  sourceInput: linkInput.sourceInput,
+                });
+              }}
+            />
+          </InputGroup>
+          <Button
+            variant="secondary"
+            onClick={() => setShowLinkModal(true)}
+            block
+          >
+            Add Link
+          </Button>{" "}
+          <br></br>
+          <br></br>
+          <br></br>
+          <Button
+            variant="secondary"
+            onClick={() => createTopology(history)}
+            block
+          >
+            Submit
+          </Button>{" "}
+          {/* <Button variant="secondary" block>
               Delete
             </Button>
             <Button variant="secondary" block>
               Go Back
             </Button>{" "} */}
-          </Col>
-          <Col>
-            <div
-              style={{
-                border: "2px solid black",
-                width: "100%",
-                height: "80vh",
-                backgroundColor: "white",
-              }}
-            >
-              <Graph
-                id="graph-id" // id is mandatory
-                data={data}
-                config={myConfig}
-                onClickNode={onClickNode}
-                onClickLink={onClickLink}
-              />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  } else {
-    return null;
-  }
+        </Col>
+        <Col>
+          <div
+            style={{
+              border: "2px solid black",
+              width: "100%",
+              height: "80vh",
+              backgroundColor: "white",
+            }}
+          >
+            <Graph
+              id="graph-id" // id is mandatory
+              data={data}
+              config={myConfig}
+              onClickNode={onClickNode}
+              onClickLink={onClickLink}
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default TopoBuilder;
